62dff513a9f3440134ed375b6ea4cd44ba3b50b6
N8Brooks <natebrooks004@gmail.com>
2019-12-08 16:34:37 -0600

    updates to algorithms

---
+++ trie.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 26 09:45:16 2019
+
+@author: Nathan
+"""
+
+
+class trie:
+    """ trie node """
+    def __init__(self):
+        self.children = dict()
+        self.isEndOfWord = False
+
+def insert(root, key):
+    """ insert into trie structure """
+    for char in key:
+        root = root.children.setdefault(char, trie())
+    root.isEndOfWord = True
+
+def search(root, key):
+    """ parse tree looking for word """
+    for char in key:
+        if char in root.children:
+            root = root.children[char]
+        else:
+            return False
+
+    return root.isEndOfWord 
+  
+# driver function 
+if __name__ == '__main__':   
+    # Input keys (use only 'a' through 'z' and lower case) 
+    keys = ["the","a","there","anaswe","any", 
+            "by","theirxy"] 
+    output = ["Not present in trie", 
+              "Present in trie"] 
+  
+    # construct trie
+    t = trie() 
+    for key in keys: 
+        insert(t, key)
+  
+    # Search for different keys 
+    print("{} ---- {}".format("the",output[search(t, "the")])) 
+    print("{} ---- {}".format("these",output[search(t, "these")])) 
+    print("{} ---- {}".format("theirx",output[search(t, "theirx")])) 
+    print("{} ---- {}".format("thaw",output[search(t, "thaw")])) 
+  
---
+++ union_find.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 26 09:45:16 2019
+
+@author: Nathan
+"""
+
+class union_find:
+    """ simple union find """
+    def __init__(self, n):
+        self.parent = list(range(n))
+
+    def find(self, x):
+        return x if self.parent[x] == x else self.find(self.parent[x])
+    
+    def unite(self, x, y):
+        self.parent[self.find(x)] = self.find(y)
+
+class union_find_count:
+    """ union find that maintains count of groups """
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.count = n
+
+    def find(self, x):
+        if (self.parent[x] == x):
+            return x
+        else:
+            y = self.find(self.parent[x])
+            self.parent[x] = y
+            return y
+    
+    def unite(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        
+        if x != y: self.count -= 1
+        self.parent[x] = y
+
+class union_find_dict:
+    """ union find that uses dictionary """
+    def __init__(self):
+        self.parent = dict()
+    
+    def find(self, x):
+        if self.parent.setdefault(x, x) == x:
+            return x
+        else:
+            return self.find(self.parent[x])
+    
+    def unite(self, x, y):
+        self.parent[self.find(x)] = self.find(y)
+
+
+    

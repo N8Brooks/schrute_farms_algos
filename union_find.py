32de8dbf55aa1b468107722fd8dc7dd8f602802f
N8Brooks <natebrooks004@gmail.com>
2019-12-08 16:46:36 -0600

    added union_find_dict_count

--- union_find.py
+++ union_find.py
@@ -23,12 +23,7 @@ class union_find_count:
         self.count = n
 
     def find(self, x):
-        if (self.parent[x] == x):
-            return x
-        else:
-            y = self.find(self.parent[x])
-            self.parent[x] = y
-            return y
+        return x if self.parent[x] == x else self.find(self.parent[x])
     
     def unite(self, x, y):
         x = self.find(x)
@@ -51,5 +46,32 @@ class union_find_dict:
     def unite(self, x, y):
         self.parent[self.find(x)] = self.find(y)
 
-
+class union_find_dict_count:
+    """ union find that maintains group count with dictionary """
+    def __init__(self):
+        self.parent = dict()
+        self.count = 0
+    
+    def find(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+            self.count += 1
+            return x
+        elif self.parent[x] == x:
+            return x
+        else:
+            return self.find(self.parent[x])
+    
+    def unite(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x != y: self.count -= 1
+        self.parent[x] = y
+    
+            
+            
+            
+            
+            
+            
     

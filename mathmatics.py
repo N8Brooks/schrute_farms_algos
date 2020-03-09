fb3e8acbf17f5db058229631d7fcae886334c62d
nathan <natebrooks004@gmail.com>
2020-03-08 20:13:18 -0500

    added more functions

--- mathmatics.py
+++ mathmatics.py
@@ -5,9 +5,13 @@ Created on Fri Jan 31 13:28:50 2020
 @author: DSU
 """
 
+from functools import reduce
+from operator import mul
+from itertools import combinations
 from math import gcd
 
 def xgcd(a, b):
+    """extended greatest common divisor"""
     """return (g, x, y) such that a*x + b*y = g = gcd(a, b)"""
     x0, x1, y0, y1 = 0, 1, 1, 0
     while a != 0:
@@ -16,29 +20,31 @@ def xgcd(a, b):
         x0, x1 = x1, x0 - q * x1
     return b, x0, y0
 
-def prime_test(n):
+def lcm(a, b):
+    """least common multiple"""
+    """return the lowest integer which both a and b divide"""
+    return abs(a * b) // gcd(a, b)
+
+def is_prime(n):
     """return if n is prime"""
     # return if it is 2 or 3
     if n < 4: return n > 1
-    
     # cut down on iteration checking 2s and 3s
     if n % 2 == 0 or n % 3 == 0: return False
-    
     # check other cases
     return all(n % i and n % (i + 2) for i in range(5, int(n ** 0.5) + 1, 6))
 
 def prime_sieve(n):
-    """Generate the primes less than ``n`` using the Sieve of Eratosthenes."""
-    a = [True] * n
-    a[0] = a[1] = False
+    """generate the primes less than n using the sieve of eratosthenes."""
+    a = [False, False] + [True] * (n - 2)
     for i, isprime in enumerate(a):
         if isprime:
             yield i
-            for j in range(i * i, n, i):
+            for j in range(i**2, n, i):
                 a[j] = False
 
 def factor(n):
-    """return factors of integer n"""
+    """return all prime factors of int n - does not include 1 and itself"""
     ret = dict()
     for prime in prime_sieve(int(n ** 0.5) + 1):
         power = 0
@@ -51,4 +57,75 @@ def factor(n):
         ret[n] = 1
     return ret
 
+def divisors(n):
+    """return all divisors of integer n"""
+    factors = list()
+    original = (1, n,)
+    for prime in prime_sieve(int(n ** 0.5) + 1):
+        while not n % prime:
+            n //= prime
+            factors.append(prime)
+    if n > 1:
+        factors.append(n)
+    return {reduce(mul, x) for i in range(1, len(factors)) \
+            for x in combinations(factors, i)}.union(original)
+
+def inverse_mod(a, m):
+    """return x such that a * x is congruent to 1 (mod m)"""
+    """can also be solved with xgcd(m, a)[2] or xgcd(a, m)[1]"""
+    a = a % m
+    return next((x for x in range(1, m) if a * x % m == 1), 1)
+
+def crt(r, m):
+    """chinese remainder theorem"""
+    """return x such that x = r (mod m) for all a and n"""
+    total = 0
+    prod = reduce(mul, m)
+    for n_i, a_i in zip(m, r):
+        p = prod // n_i
+        total += a_i * xgcd(n_i, p)[2] * p
+    return total % prod
+
+def bsgs(g, r, p):
+    """baby step giant step algorithm"""
+    """return x such that g^x = r (mod p)"""
+    n = int((p-1) ** 0.5) + 1
+    u = xgcd(p, pow(g, n, p))[2] % p
     
+    def powers(base, multiplier):
+        yield base
+        for _ in range(n):
+            base = base * multiplier % p
+            yield base
+    
+    list1 = {tmp:i for i, tmp in enumerate(powers(1, g))}
+    list2 = enumerate(powers(r,u))
+    
+    return next(list1[tmp] + j * n for j, tmp in list2 if tmp in list1)
+
+def pha(g, r, p):
+    """pohlig-hellman algorithm"""
+    """return x such that g^x = r (mod p)"""
+    u = p - 1
+    ms = [base**exp for base, exp in factor(u).items()]
+    rs = [bsgs(pow(g, x, p), pow(r, x, p), p) for x in map(lambda x: u//x, ms)]
+    return crt(rs, ms)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pseudorandom.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  1 09:35:06 2020
+
+@author: nathan
+"""
+
+from random import randrange
+
+def is_prime(n):
+    """return if n is prime"""
+    # return if it is 2 or 3
+    if n < 4: return n > 1
+    # cut down on iteration checking 2s and 3s
+    if n % 2 == 0 or n % 3 == 0: return False
+    # check other cases
+    return all(n % i and n % (i + 2) for i in range(5, int(n ** 0.5) + 1, 6))
+
+def get_prime(n):
+    a, b = 10**(n - 1), 10**n
+    while True:
+        x = randrange(a, b)
+        if is_prime(x): return x
+
+def linear_recurrence(seed, a, x, n):
+    while True:
+        seed = (a * seed + c) % n
+        yield seed
+
+def square_last_term(seed, n=get_prime(8) * get_prime(8)):
+    while True:
+        seed = pow(seed, 2, n)
+        yield seed
+
+def square_the_middle(seed, n=4):
+    half = 10 ** (n // 2)
+    max_digit = 10 ** n
+    min_digit = 10 ** (2 * n - 1)
+    while True:
+        seed **= 2
+        while 0 < seed < min_digit:
+            seed *= 10
+        seed = seed // half % max_digit
+        yield seed
